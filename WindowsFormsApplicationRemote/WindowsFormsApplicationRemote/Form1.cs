45049a0b1776226bfec46cc2437fcb6f53a2ffe8
Andrés Palomares <andres.palomares@spenta.es>
2012-07-06 12:11:08 +0200

    Otro commit.

--- WindowsFormsApplicationRemote/WindowsFormsApplicationRemote/Form1.cs
+++ WindowsFormsApplicationRemote/WindowsFormsApplicationRemote/Form1.cs
@@ -15,5 +15,10 @@ namespace WindowsFormsApplicationRemote
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
