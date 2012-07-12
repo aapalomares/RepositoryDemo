8dee4845bbca2f70581461651ebd236dc3dce322
Andrés Palomares <andres.palomares@spenta.es>
2012-07-12 12:21:50 +0200

    Añadido test NUnit.

--- WindowsFormsApplicationRemote/WindowsFormsApplicationRemote/WindowsTest.cs
+++ WindowsFormsApplicationRemote/WindowsFormsApplicationRemote/WindowsTest.cs
@@ -2,10 +2,38 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using NUnit.Framework;
+using System.Threading;
+using System.Diagnostics;
 
 namespace WindowsFormsApplicationRemote
 {
+    [TestFixture]
     class WindowsTest
     {
+        [Test]
+        public void TestSuccess()
+        {
+            Trace.TraceInformation("Test trace.");
+        }
+
+        [Test]
+        public void TestException()
+        {
+            throw new Exception("Test Exception");
+        }
+
+        [Test]
+        public void TestAssert()
+        {
+            Assert.Fail("Test Assert");
+        }
+
+        [Test]
+        public void TestHang()
+        {
+            Thread.Sleep(10000);
+        }
+
     }
 }
