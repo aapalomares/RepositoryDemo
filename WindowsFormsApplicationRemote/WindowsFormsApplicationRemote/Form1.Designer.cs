d60cf0937d64379ac5de6e086eafd3d7d9eece02
Andrés Palomares <andres.palomares@spenta.es>
2012-07-13 08:49:38 +0200

    Nuevo

--- WindowsFormsApplicationRemote/WindowsFormsApplicationRemote/Form1.Designer.cs
+++ WindowsFormsApplicationRemote/WindowsFormsApplicationRemote/Form1.Designer.cs
@@ -33,7 +33,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(139, 79);
+            this.button1.Location = new System.Drawing.Point(137, 109);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
