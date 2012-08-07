29729c994c740bfb563dadafea248a5eceadc796
Andres Palomares <andres.palomares@spenta.es>
2012-08-07 12:58:55 +0200

    Añadido Proyecto MSBuild

---
+++ MSBuildProject/MSBuildProjectClass/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MSBuildProjectClass
+{
+    public class Class1
+    {
+        private string _someVar;
+        private const string ABC = "Abc";
+
+        public Class1(string val)
+        {
+            _someVar = val;
+        }
+    }
+}
---
+++ MSBuildProject/MSBuildProjectTest/MSBuildTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using System.Diagnostics;
+using System.Threading;
+
+namespace MSBuildProjectTest
+{
+    [TestFixture]
+    public class MSBuildTest
+    {
+        [Test]
+        public void TestSuccess()
+        {
+            Trace.TraceInformation("Test trace.");
+        }
+
+        //[Test]
+        //public void TestException()
+        //{
+        //    throw new Exception("Test Exception");
+        //}
+
+        //[Test]
+        //public void TestAssert()
+        //{
+        //    Assert.Fail("Test Assert");
+        //}
+
+        [Test]
+        public void TestHang()
+        {
+            Thread.Sleep(10000);
+        }
+
+    }
+}
